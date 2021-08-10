d78b59a7072b0978eb034cd0c0c257a0a2136635
Jasjeev <jasjeev4@gmail.com>
2021-08-10 14:20:16 -0400

    Create MVVM structure for app

---
+++ UnsplashedGallery/Models/UnsplashedModel.swift
@@ -0,0 +1,8 @@
+//
+//  UnsplashedModel.swift
+//  UnsplashedGallery
+//
+//  Created by Jasjeev on 8/10/21.
+//
+
+import Foundation
---
+++ UnsplashedGallery/ViewModels/UnsplashedViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UnsplashedViewModel.swift
+//  UnsplashedGallery
+//
+//  Created by Jasjeev on 8/10/21.
+//
+
+import Foundation
