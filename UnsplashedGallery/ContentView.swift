9b0c01cec6f049d0a5f285296b5129805cb81b4e
Jasjeev <jasjeev4@gmail.com>
2021-08-09 22:18:40 -0400

    Initial Commit

---
+++ UnsplashedGallery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnsplashedGallery
+//
+//  Created by Jasjeev on 8/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
