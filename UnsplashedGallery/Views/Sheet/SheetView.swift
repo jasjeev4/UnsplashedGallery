87b2849979988a7cdaaa86b7575c46ec8a920d26
Jasjeev <jasjeev4@gmail.com>
2021-08-11 01:46:00 -0400

    Finalize Sheet

---
+++ UnsplashedGallery/Views/Sheet/SheetView.swift
@@ -0,0 +1,20 @@
+//
+//  SheetView.swift
+//  UnsplashedGallery
+//
+//  Created by Jasjeev on 8/11/21.
+//
+
+import SwiftUI
+
+struct SheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        SheetView()
+    }
+}
